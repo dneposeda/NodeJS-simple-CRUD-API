6cab287a390176e0e73f38d614ba10d9bab8fa1a
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-19 21:40:03 +0400

    added api

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import dotenv from "dotenv";
-import http from "http";
+import { createServer } from "http";
+import { notFoundUrl } from "./api/not-found-url";
+import { methods } from "./api/users/controllers";
 
 dotenv.config();
 
-const message = "Hello World!";
+const port = process.env.PORT || 8080;
 
-http.createServer(function(request,response){
+const userApi = '/api/users';
 
-    console.log(message);
-    response.end(message);
+const server = createServer();
 
-}).listen(process.env.PORT,  ()=>{
-    console.log("Сервер начал прослушивание запросов");
+server.on('request', (req, res) => {
+    const url = req.url || '';
+    const method = req.method || '';
+
+    if (!url.startsWith(userApi)) {
+        return notFoundUrl(res)
+    }
+
+    try {
+        methods[method](req, res);
+    } catch (e) {
+        notFoundUrl(res)
+    }
 });
+
+server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(port,  () => console.log(`HTTP is listening on ${ port }`));
